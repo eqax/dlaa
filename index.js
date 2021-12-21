ea9c1e2359d4a7fece6b8663f6b70d6b0e917c3f
Glitch (scalloped-mahogany-chronometer) <none>
2021-12-21 19:36:22 +0000

    🦄💅 Checkpoint ./index.js:5578687/1831

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const fs = require("fs");
+let fetch = require('node-fetch')
 var discordIndexHTML = fs.readFileSync(__dirname + "/index.html", { encoding: "utf8" });
 
 app.use("*", async (req, res, next) => {
@@ -9,10 +10,41 @@ app.use("*", async (req, res, next) => {
 });
 
 app.use("/src", express.static(__dirname + "/src"));
-app.use("/*", async (req, res) => {
-  data
+app.use("/:data.:data2", async (req, res) => {
+  let dataFetch = await fetch('https://discord.com/' + req.params.data + "." + req.params.data2)
+  let dataText = await dataFetch.text()
+  console.log('https://discord.com/'+ req.params.data + "." + req.params.data2)
+  try {
+  
+    let s = JSON.parse(dataText)
+    
+    return res.json(s)
+    
+  } catch {
+return res.send(dataText)
+}
+  
 	res.send();
 });
+
+app.use("/:data", async (req, res) => {
+  let dataFetch = await fetch('https://discord.com/' + req.params.data)
+  let dataText = await dataFetch.text()
+  console.log('https://discord.com/' + req.params.data)
+  try {
+  
+    let s = JSON.parse(dataText)
+    
+    return res.json(s)
+    
+  } catch {
+return res.send(dataText)
+}
+  
+	res.send();
+});
+
+
 
 app.use("/", async (req, res) => {
 	res.send(discordIndexHTML);
