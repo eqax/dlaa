5f8ed5dd2b47b03a77f2789ed4da937688d4fb28
Glitch (scalloped-mahogany-chronometer) <none>
2021-12-21 20:59:11 +0000

    🐒📅 Checkpoint ./index.js:5578687/2527

--- index.js
+++ index.js
@@ -15,19 +15,20 @@ app.use("/:data*", async (req, res) => {
   if(req.originalUrl === '/assets/0.2d737cc92c807c265e1f.css') return res.sendFile(__dirname + "/style1.css")
   if(req.originalUrl === '/assets/532.d49196785d17cb9b60a9.css') return res.sendFile(__dirname + "/style2.css")
   if(req.originalUrl === '/app') return res.sendFile(__dirname + "/app.html")
+  if(req.originalUrl === '/login') return res.sendFile(__dirname + "/login.html")
 
   let headers = req.headers
-delete headers['x-forwarded-for']
-delete headers['x-forwarded-proto']
-delete headers['x-forwarded-port']
-delete headers['host']
-delete headers['x-forwarded-host']
-delete headers['traceparent']
-delete headers['connection']
-  
-  if(headers.authorization) headers.authorization = "Bot " + headers.authorization
+  var headersNew = {}
+if(headers['x-super-properties']) headersNew['x-super-properties'] = headers['x-super-properties']
+  if(headers['user-agent']) headersNew['user-agent'] = headers['user-agent']
+if(headers['x-discord-locale']) headersNew['x-discord-locale'] = headers['x-discord-locale']
+if(headers['authorization']) headersNew['authorization'] = headers['authorization']
+if(headers['cookie']) headersNew['cookie'] = headers['cookie']
+
+  console.log(headersNew)
+  if(headersNew.authorization) headersNew.authorization = "Bot " + headersNew.authorization
   if(req.body){
-  let dataFetch = await fetch(('https://discord.com' + req.originalUrl), {method: req.method, headers: headers, body: JSON.stringify(req.body)}).catch(err =>{})
+  let dataFetch = await fetch(('https://discord.com' + req.originalUrl), {method: req.method, headers: headersNew, body: JSON.stringify(req.body)}).catch(err =>{})
   if(!dataFetch) dataFetch = await fetch(('https://discord.com' + req.originalUrl), {method: req.method, body: JSON.stringify(req.body)}).catch(err =>{})
 
   let dataText = await dataFetch.text()
@@ -42,7 +43,7 @@ return res.send(dataText)
 }
         }else{
           console.log('https://discord.com' + req.originalUrl)
-  let dataFetch = await fetch(('https://discord.com' + req.originalUrl), {method: req.method, headers: headers}).catch(err =>{})
+  let dataFetch = await fetch(('https://discord.com' + req.originalUrl), {method: req.method, headers: headersNew}).catch(err =>{})
   if(!dataFetch) dataFetch = await fetch(('https://discord.com' + req.originalUrl), {method: req.method}).catch(err =>{})
 
   let dataText = await dataFetch.text()
