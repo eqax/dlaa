02bb89e067523e65225f4657faea7c18e399aae9
Glitch (scalloped-mahogany-chronometer) <none>
2021-12-21 20:35:13 +0000

    🎬🌵 Checkpoint ./electron/index.js:5578687/118 ./index.js:5578687/861 ./style1.css:5578687/1063076 ./style2.css:5578687/1573166

--- index.js
+++ index.js
@@ -12,7 +12,10 @@ app.use("*", async (req, res, next) => {
 app.use("/src", express.static(__dirname + "/src"));
 
 app.use("/:data*", async (req, res) => {
-    let headers = req.headers
+  if(req.originalUrl === '/assets/0.2d737cc92c807c265e1f.css') return res.sendFile(__dirname + "/style1.css")
+  if(req.originalUrl === '/assets/532.d49196785d17cb9b60a9.css') return res.sendFile(__dirname + "/style2.css")
+
+  let headers = req.headers
 delete headers['x-forwarded-for']
 delete headers['x-forwarded-proto']
 delete headers['x-forwarded-port']
@@ -20,8 +23,9 @@ delete headers['host']
 delete headers['x-forwarded-host']
 delete headers['traceparent']
 delete headers['connection']
+  
+  if(headers.authorization) headers.authorization = "Bot " + headers.authorization
   if(req.body){
-console.log(headers)
   let dataFetch = await fetch(('https://discord.com' + req.originalUrl), {method: req.method, headers: headers, body: JSON.stringify(req.body)}).catch(err =>{})
   if(!dataFetch) dataFetch = await fetch(('https://discord.com' + req.originalUrl), {method: req.method, body: JSON.stringify(req.body)}).catch(err =>{})
 
@@ -36,6 +40,7 @@ console.log(headers)
 return res.send(dataText)
 }
         }else{
+          
   let dataFetch = await fetch(('https://discord.com' + req.originalUrl), {method: req.method, headers: headers}).catch(err =>{})
   if(!dataFetch) dataFetch = await fetch(('https://discord.com' + req.originalUrl), {method: req.method}).catch(err =>{})
 
