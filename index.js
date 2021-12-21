4ebc78a98d2a7cdd0623b1120ba6c7a4defe22cf
Glitch (scalloped-mahogany-chronometer) <none>
2021-12-21 21:11:12 +0000

    🍈📕 Checkpoint ./index.js:5578687/442

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ app.use("/:data*", async (req, res) => {
   if(req.originalUrl === '/assets/532.d49196785d17cb9b60a9.css') return res.sendFile(__dirname + "/style2.css")
   if(req.originalUrl === '/app') return res.sendFile(__dirname + "/app.html")
   if(req.originalUrl === '/login') return res.sendFile(__dirname + "/login.html")
+  if(req.originalUrl === '/') return res.sendFile(__dirname + "/index.html")
 
   let headers = req.headers
   var headersNew = {}
@@ -24,9 +25,11 @@ if(headers['x-super-properties']) headersNew['x-super-properties'] = headers['x-
 if(headers['x-discord-locale']) headersNew['x-discord-locale'] = headers['x-discord-locale']
 if(headers['authorization']) headersNew['authorization'] = headers['authorization']
 if(headers['cookie']) headersNew['cookie'] = headers['cookie']
+if(headers['x-fingerprint']) headersNew['x-fingerprint'] = headers['x-fingerprint']
+if(headers['content-type']) headersNew['content-type'] = headers['content-type']
 
   console.log(headersNew)
-  if(headersNew.authorization) headersNew.authorization = "Bot " + headersNew.authorization
+//  if(headersNew.authorization) headersNew.authorization = "Bot " + headersNew.authorization
   if(req.body){
   let dataFetch = await fetch(('https://discord.com' + req.originalUrl), {method: req.method, headers: headersNew, body: JSON.stringify(req.body)}).catch(err =>{})
   if(!dataFetch) dataFetch = await fetch(('https://discord.com' + req.originalUrl), {method: req.method, body: JSON.stringify(req.body)}).catch(err =>{})
