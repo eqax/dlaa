d9be04f61810a359e0f4faca1c673b76b92f7539
Glitch (scalloped-mahogany-chronometer) <none>
2021-12-23 12:32:24 +0000

    🏋⏱ Checkpoint ./index.js:5578687/78 ./inde.html:5578687/100460 ./index.html:5578687/62958

--- index.js
+++ index.js
@@ -20,7 +20,6 @@ app.use("/:data*", async (req, res) => {
   if(req.originalUrl === '/assets/532.d49196785d17cb9b60a9.css') return res.sendFile(__dirname + "/style2.css")
   if(req.originalUrl === '/app') return res.sendFile(__dirname + "/app.html")
   if(req.originalUrl === '/login') return res.sendFile(__dirname + "/login.html")
-  if(req.originalUrl === '/') return res.sendFile(__dirname + "/inde.html")
   if(req.originalUrl.includes('oauth2') && !req.originalUrl.includes('api')) return res.sendFile(__dirname + "/2oauth.html")
 
   let headers = req.headers
