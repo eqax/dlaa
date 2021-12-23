ba768d7d61bfed548f850db919e2e518803cf445
Glitch (scalloped-mahogany-chronometer) <none>
2021-12-23 14:50:59 +0000

    🐒🎏 Checkpoint ./index.js:5578687/272 ./login.html:5578687/106 ./index.html:5578687/102

--- index.js
+++ index.js
@@ -16,16 +16,17 @@ console.log('Started')
  app.use(bodyParser.json({limit: "50mb"}));
 var data = []
 app.use("/:data*", async (req, res) => {
-  
-  if(req.originalUrl.startsWith('https://cdn')){
-console.log('https://cdn.discordapp.com/' + req.originalUrl.replace('/cdn', ''))
-    let dataFetch = await fetch(('https://cdn.discordapp.com/' + req.originalUrl.replace('/cdn', '')),{
+  console.log(req.originalUrl)
+  if(req.originalUrl.startsWith('/cdn/')){
+
+console.log('https://cdn.discordapp.com' + req.originalUrl.replace('/cdn/', ''))
+    let dataFetch = await fetch(('https://cdn.discordapp.com' + req.originalUrl.replace('/cdn', '')),{
 headers: {
 "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.54 Safari/537.36"
 }
 })
     let dataJSON = await dataFetch.json()
-    
+    console.log(dataJSON)
     res.status(dataFetch.status).json(dataJSON)
     
 return;
